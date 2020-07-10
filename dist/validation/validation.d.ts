405e2f4a52636d6c8b7de326bf520f5a1bdec61b
dikac <rigel234@yahoo.com>
2020-07-10 13:18:17 +0700

    add template Return to validation/validation

--- dist/validation/validation.d.ts
+++ dist/validation/validation.d.ts
@@ -1,4 +1,4 @@
 import Function from "@dikac/t-function/function";
-export default interface Validation<Arguments extends any[]> {
-    validation: Function<Arguments, boolean>;
+export default interface Validation<Arguments extends any[], Return extends boolean = boolean> {
+    validation: Function<Arguments, Return>;
 }
--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -1,6 +1,6 @@
 import Function from "@dikac/t-function/function";
 
-export default interface Validation<Arguments extends any[]> {
+export default interface Validation<Arguments extends any[], Return extends boolean = boolean> {
 
-    validation : Function<Arguments, boolean>;
+    validation : Function<Arguments, Return>;
 }
