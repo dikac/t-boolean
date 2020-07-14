fc76a872267fe54768b9ddfd7e83b747e83514f6
dikac <rigel234@yahoo.com>
2020-07-14 07:05:21 +0700

    bump version

--- dist/and.d.ts
+++ dist/and.d.ts
@@ -1,4 +1,4 @@
-import { UnionToIntersection } from "utility-types";
+import { Union } from "ts-toolbelt";
 declare type Validator<V extends any[]> = {
     [P in keyof V]: (value: any) => value is V[P];
 };
@@ -10,5 +10,5 @@ declare type TupleTypes<T> = {
 } extends {
     [key: number]: infer V;
 } ? V : never;
-export default function And<V extends any[]>(value: any, ...validators: Validator<V>): value is UnionToIntersection<TupleTypes<V>>;
+export default function And<V extends any[]>(value: any, validators: Validator<V>): value is Union.IntersectOf<TupleTypes<V>>;
 export {};
